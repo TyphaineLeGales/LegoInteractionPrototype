14c045611dbd23813857d75e430b882e9cdc932e
TyphaineLeGales <tiphaine.legales@neuf.fr>
2020-02-20 18:07:29 +0100

    fixed iteration through texture

--- CameraTexture.cs
+++ CameraTexture.cs
@@ -6,9 +6,11 @@ public class CameraTexture : MonoBehaviour
 {
     static WebCamTexture camTex;
     private Texture2D rasterizedTex;
-    public Color[] pixels;
-    public Color[] newPixelArray;
-    private int texScale = 2;
+    private Color[] pixels;
+    private Color[] newPixelArray;
+
+    public int texScale = 10;
+
    
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,6 @@ public class CameraTexture : MonoBehaviour
         if(camTex == null)
         {
             camTex = new WebCamTexture(); //input height = 720 iput width=1280 16:9 ratio length of pixel array 256
-            rasterizedTex = new Texture2D(camTex.width/texScale, camTex.height/texScale); //Array of 1024 pixels
             pixels = camTex.GetPixels();
             
         }
@@ -26,26 +27,28 @@ public class CameraTexture : MonoBehaviour
             camTex.Play();
         }
         
-        GetComponent<Renderer>().material.mainTexture = rasterizedTex ;
         
+        rasterizedTex = new Texture2D(camTex.width/texScale, camTex.height/texScale); //Array of 1024 pixels
+        newPixelArray = new Color[pixels.Length/texScale];
+
+        GetComponent<Renderer>().material.mainTexture = rasterizedTex ;
+        //Debug.Log(rasterizedTex.width);
     }
 
     // Update is called once per frame
     void Update()
     {
         pixels = camTex.GetPixels();
-        newPixelArray = new Color[pixels.Length/texScale];
 
-        for(int x = 0 ; x<newPixelArray.Length; x+=1){
-
-            newPixelArray[x] = pixels[x*texScale];
-        }
+        for(int y = 0; y< rasterizedTex.height; y++){
+           for(int x = 0; x< rasterizedTex.width; x++){
+               Color color = camTex.GetPixel(x*texScale, y*texScale);
+               rasterizedTex.SetPixel(x, y, color);
             
-        rasterizedTex.SetPixels(newPixelArray);
+            }  
+        } 
+
         rasterizedTex.Apply();
-        
-        // Debug.Log("rasterized tex :" + rasterizedTex.GetPixel(1, 1));
-        // Debug.Log("original tex :"+  camTex.GetPixel(1, 1));
-        
+          
     }
 }
