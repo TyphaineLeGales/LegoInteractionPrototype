415d73aa97d8c0bb38cc6160b14759b1161d759b
TyphaineLeGales <tiphaine.legales@neuf.fr>
2020-02-21 11:11:14 +0100

    unefficient way of creating cubes with pixel color

--- CameraTexture.cs
+++ CameraTexture.cs
@@ -8,9 +8,9 @@ public class CameraTexture : MonoBehaviour
     private Texture2D rasterizedTex;
     private Color[] pixels;
     private Color[] newPixelArray;
-    public bool rasterized;
 
-    public int texScale = 10;
+    public bool rasterized;
+    public int texScale = 32;
 
     void Start()
     {
@@ -25,7 +25,7 @@ public class CameraTexture : MonoBehaviour
             camTex.Play();
         }
         
-        rasterizedTex = new Texture2D(72, 128); //Array of 1024 pixels
+        rasterizedTex = new Texture2D(camTex.width/texScale, camTex.height/texScale); //Array of 1024 pixels
         newPixelArray = new Color[pixels.Length/texScale];
 
         if(rasterized == true) {
@@ -33,7 +33,7 @@ public class CameraTexture : MonoBehaviour
         } else {
             GetComponent<Renderer>().material.mainTexture = camTex ;
         }
-        //Debug.Log(rasterizedTex.width);
+
     }
 
     void Update()
@@ -44,11 +44,14 @@ public class CameraTexture : MonoBehaviour
            for(int x = 0; x< rasterizedTex.width; x++){
                Color color = camTex.GetPixel(x*texScale, y*texScale);
                rasterizedTex.SetPixel(x, y, color);
-    
+            //    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //    cube.transform.position = new Vector3(x*16,0, y*9);
+            //    cube.transform.localScale = new Vector3(1, 1, 1);
+            //    cube.GetComponent<Renderer>().material.color = color;
+
             }  
         } 
 
-        rasterizedTex.Apply();
-          
+        rasterizedTex.Apply();      
     }
 }
